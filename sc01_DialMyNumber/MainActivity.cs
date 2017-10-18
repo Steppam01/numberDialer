0fd744feb353cd498d7e18bd7ff93f0fef6bd331
Stephen <stephen.pamphile001@mymdc.net>
2017-10-17 20:44:39 -0400

    Number app

--- sc01_DialMyNumber/MainActivity.cs
+++ sc01_DialMyNumber/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace sc01_DialMyNumber
 {
@@ -13,6 +14,17 @@ namespace sc01_DialMyNumber
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button dialButton = FindViewById<Button>(Resource.Id.btnDial);
+
+            dialButton.Click += delegate
+            {
+                EditText phoneNum = FindViewById<EditText>(Resource.Id.phoneNumber);
+                string phoneNumber = phoneNum.Text; 
+                var uri = Android.Net.Uri.Parse("tel:" + phoneNumber);
+                Intent myIntent = new Intent(Intent.ActionDial, uri);
+                StartActivity(myIntent);
+            };
         }
     }
 }
